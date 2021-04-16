7b8e12197aa14c919bf19a330d9208eb9457312c
Derek Murphy <derekmurphy1993@gmail.com>
2021-04-16 17:53:19 -0400

    section 4

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem'
+import Card from './components/Card'
 
 function App() {
+  const expenses = [
+    { title: 'car insurnance', amount: 3223, date: new Date(2021, 2, 28) },
+    { title: 'cat insurnance', amount: 40.23, date: new Date(2021, 2, 26) },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className="App">
+     <ExpenseItem 
+     title={expenses[0].title}
+     amount={expenses[0].amount}
+     date={expenses[0].date}
+      > </ExpenseItem>
+         <ExpenseItem 
+     title={expenses[1].title}
+     amount={expenses[1].amount}
+     date={expenses[1].date}
+      > </ExpenseItem>
+    </Card>
   );
 }
 
