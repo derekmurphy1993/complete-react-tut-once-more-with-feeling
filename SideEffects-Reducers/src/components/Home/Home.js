bae5518b0defe6acb39a8c6b946fe1449c067a88
Derek Murphy <derekmurphy1993@gmail.com>
2021-05-24 23:43:12 -0400

    Context Complete

--- SideEffects-Reducers/src/components/Home/Home.js
+++ SideEffects-Reducers/src/components/Home/Home.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
+import { useCallback } from 'react';
+import AuthContext from '../../store/auth-context';
 
 const Home = (props) => {
+  const authCtx = useContext(AuthContext)
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <button onClick={authCtx.onLogout}>Logout</button>
     </Card>
   );
 };
